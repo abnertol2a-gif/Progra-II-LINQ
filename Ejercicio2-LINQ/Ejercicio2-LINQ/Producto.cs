6b75383bef374e41dbeb346b9bf185ec3d6f2a69
abnertol2a-gif <abnertol2a@gmail.com>
2025-10-25 22:43:09 -0600

    Commit inicial. Se crea el archivo de datos y la clase producto

---
+++ Ejercicio2-LINQ/Ejercicio2-LINQ/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio2_LINQ
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Categoria { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Nombre} ({Categoria}) | Precio: {Precio:C} | Stock: {Stock}";
+        }
+    }
+}
